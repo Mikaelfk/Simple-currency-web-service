733d9147d617085e7472ed9f4eb725febb31c90a
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-02-13 17:10:36 +0100

    Made a simple web server

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func handler() {
+	http.HandleFunc("/exchange/v1/exchangehistory/", exchangehistory)
+	http.HandleFunc("/exchange/v1/exchangeborder/", exchangeborder)
+	http.HandleFunc("/exchange/v1/diag/", diag)
+}
+
+func main() {
+	handler()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func exchangehistory(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Exchange History Endpoint")
+}
+
+func exchangeborder(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Exchange border Endpoint")
+}
+
+func diag(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Diag Endpoint")
 }
