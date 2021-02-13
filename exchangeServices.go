435cae3926ee83e0668836f82d15f57eeae59fcf
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-02-14 00:58:45 +0100

    Added JSON parsing

--- exchangeServices.go
+++ exchangeServices.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type Currency struct {
+	Code   string
+	Name   string
+	Symbol string
+}
+
+type Information struct {
+	Currencies []Currency
+	Borders    []string
+}
 
 /*
  * A function which retrieves the exchange history of a currency of a requested country
@@ -16,8 +28,10 @@ func exchangehistory(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Exchange History Endpoint")
 	vars := mux.Vars(r)
 	countryName := vars["country_name"]
-	resp, err := http.Get("https://restcountries.eu/rest/v1/name/" + countryName + "?fields=borders;currencies")
+	resp, err := http.Get("https://restcountries.eu/rest/v2/name/" + countryName + "?fields=borders;currencies")
 	body, err := ioutil.ReadAll(resp.Body)
+
+	var information []Information
 
 	if resp.StatusCode != 200 {
 		fmt.Fprint(w, "Error")
@@ -25,8 +39,9 @@ func exchangehistory(w http.ResponseWriter, r *http.Request) {
 		// handle error
 		fmt.Fprint(w, "Error")
 	} else {
-		fmt.Println(vars)
-		fmt.Fprint(w, string(body))
+		fmt.Fprint(w, string(body)+"\n")
+		json.Unmarshal([]byte(string(body)), &information)
+		fmt.Fprint(w, information)
 	}
 }
 
