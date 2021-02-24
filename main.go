cdb6852be71046b2b31b6a310f594c76b7aa360c
Mikael Falkenberg Krog <mikael.f.krog@gmail.com>
2021-02-24 11:13:38 +0100

    Application now logs what port is used at the start

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ var startTime time.Time
  * A handler function for handling what functions are called when the different urls are visited
  */
 func handler() {
-	startTime = time.Now()
 	r := mux.NewRouter()
 	r.HandleFunc("/exchange/v1/exchangehistory/{country_name}/{begin_date-end_date}", exchangehistory)
 	r.HandleFunc("/exchange/v1/exchangeborder/{country_name}", exchangeborder).Queries("limit", "{limit}")
@@ -29,11 +28,13 @@ func handler() {
  * Main function that initialized the application
  */
 func main() {
+	startTime = time.Now()
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 	handler()
+	log.Printf("Listening on port :%s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
